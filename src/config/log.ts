5b72d2fe4db10b2a794a0fb19155a6c8ac8b9723
whatkimkong <whatkimkong@gmail.com>
2022-04-18 17:42:07 +0200

    feat: logging functions

---
+++ src/config/log.ts
@@ -0,0 +1,42 @@
+const getTimeStamp = (): string => {
+    return new Date().toISOString();
+};
+
+const info = (namespace: string, message: string, obj?: any) => {
+    if (obj) {
+        console.log(`[${getTimeStamp()}] [INFO] [${namespace}] ${message}`, obj)
+    } else {
+        console.log(`[${getTimeStamp()}] [INFO] [${namespace}] ${message}`)
+    }
+}
+
+const warn = (namespace: string, message: string, obj?: any) => {
+    if (obj) {
+        console.warn(`[${getTimeStamp()}] [WARN] [${namespace}] ${message}`, obj)
+    } else {
+        console.warn(`[${getTimeStamp()}] [WARN] [${namespace}] ${message}`)
+    }
+}
+
+const error = (namespace: string, message: string, obj?: any) => {
+    if (obj) {
+        console.error(`[${getTimeStamp()}] [ERROR] [${namespace}] ${message}`, obj)
+    } else {
+        console.error(`[${getTimeStamp()}] [ERROR] [${namespace}] ${message}`)
+    }
+}
+
+const debug = (namespace: string, message: string, obj?: any) => {
+    if (obj) {
+        console.debug(`[${getTimeStamp()}] [DEBUG] [${namespace}] ${message}`, obj)
+    } else {
+        console.debug(`[${getTimeStamp()}] [DEBUG] [${namespace}] ${message}`)
+    }
+}
+
+export default {
+    info,
+    warn,
+    error,
+    debug
+}
