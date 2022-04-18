0e0ee307c21a4e4067333e189e6611b5493913b4
whatkimkong <whatkimkong@gmail.com>
2022-04-18 18:27:25 +0200

    add: first sample controller for testing

---
+++ src/controllers/sample.ts
@@ -0,0 +1,14 @@
+import { Request, Response, NextFunction } from 'express';
+import log from '../config/log';
+
+const NAMESPACE = 'Sample Controller';
+
+const sampleHealthCheck = (req: Request, res: Response, next: NextFunction) => {
+    log.info(NAMESPACE, `Sample health check route called.`);
+    
+    return res.status(200).json({
+        message: 'pong'
+    })
+}
+
+export { sampleHealthCheck };
